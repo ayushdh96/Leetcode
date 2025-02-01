d0c7d11631a9e5eba7753f57afae55170b63277b
Ayush Dhoundiyal <53572847+ayushdh96@users.noreply.github.com>
2025-02-01 11:40:23 -0500

    Time: 0 ms (100.00%) | Memory: 19.8 MB (60.83%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,15 +1,45 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        map<int, int> mpp;
-        vector<int> variables;
+        int counter1=0, counter2=0, c1=0,c2=0;;
         for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]+=1;
-            if(mpp[nums[i]]==((nums.size()/3)+1)){
-                variables.push_back(nums[i]);
-                
+            if(c1==0 && counter2!=nums[i]){
+                counter1=nums[i];
+                c1=1;
+            }
+            else if(c2==0 && counter1!=nums[i]){
+                counter2=nums[i];
+                c2=1;
+            }
+            else if(counter1==nums[i]){
+                c1++;
+            }
+            else if(counter2==nums[i]){
+                c2++;
+            }
+            else{
+                c1--;
+                c2--;
             }
         }
-        return variables;
+        c1=0;c2=0;
+        for(int i=0;i<nums.size();i++){
+            if(counter1==nums[i]){
+                c1++;
+            }
+            else if(counter2==nums[i]){
+                c2++;
+            }}
+        cout<<c1<<" "<<c2<<" "<<counter1<<" "<<counter2<<" "<<nums.size()/3;
+        if(c1>nums.size()/3 && c2<=nums.size()/3){
+            return {counter1};
+        }
+        else if(c1<=nums.size()/3 && c2>nums.size()/3){
+            return {counter2};
+        }
+        else if(c1>nums.size()/3 && c2>nums.size()/3){
+            return{counter1,counter2};
+        }
+        return{};
     }
 };
