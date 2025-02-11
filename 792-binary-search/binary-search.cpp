5715dcf955d36fd55d9226c2efbd385eec2ba06d
Ayush Dhoundiyal <53572847+ayushdh96@users.noreply.github.com>
2025-02-11 00:28:32 -0500

    Time: 0 ms (100.00%) | Memory: 31.4 MB (45.15%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0,last=nums.size()-1;
+        int value=recur(nums,l,last,target);
+        return value;
+    }
+
+    int recur(vector<int>& nums,int l, int last ,int target){
+        int mid=(l+last)/2;
+        if(l>last){return -1;}
+        else if(nums[mid]==target){ return mid;}
+        else if(nums[mid]>target){return recur(nums,l,mid-1,target);}
+        else {return recur(nums,mid+1,last,target);}
+    }
+};
