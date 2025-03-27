01b1a8f8c39c11f199ae29e46b296f8c80594ec5
Ayush Dhoundiyal <ayushdh96@gmail.com>
2025-03-27 19:28:44 -0400

    Time: 27 ms (87.60%) | Memory: 70.7 MB (62.76%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -5,26 +5,14 @@ public:
             return points;
         }
         vector<vector<int>>ans;
-        /**vector<double> distance;
-        vector<vector<int>> ans;
-        for(int i=0;i<points.size();i++){
-            distance.push_back(sqrt((points[i][0]*points[i][0]) + (points[i][1]*points[i][1])));
-        }
-        vector<double> distanceCopy=distance;
-        sort(distanceCopy.begin(),distanceCopy.end());
-        double cutOff=distanceCopy[k-1];
-        for(int i=0;i<distance.size();i++){
-            if(distance[i]<=cutOff){
-                ans.push_back(points[i]);
-            }
-        }**/
         priority_queue<pair<double,pair<int,int>>,vector<pair<double, pair<int,int>>>,greater<pair<double,pair<int,int>>>> q;
         for(int i=0;i<points.size();i++){
             q.push({sqrt((points[i][0]*points[i][0]) + (points[i][1]*points[i][1])),{points[i][0],points[i][1]}});
         }
         for(int i = 0; i < k; i++){
-    auto [x, y] = q.top().second;
-    ans.push_back({x, y});
+    int x=q.top().second.first;
+    int y=q.top().second.second;
+    ans.push_back({x,y});
     q.pop();
 }
         return ans;
