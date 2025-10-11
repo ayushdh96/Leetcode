04b9945f0b07e637f35f7a5168b34dd703d4666b
Ayush Dhoundiyal <ayushdh96@gmail.com>
2025-10-11 12:32:16 -0400

    Time: 0 ms (100.00%) | Memory: 10.7 MB (46.59%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,7 +2,6 @@ class Solution {
 private:
     int houseRobber(int idx, vector<int>& dp, vector<int>& nums){
         if(idx<0) return 0;
-        else if(idx==0) return nums[idx];
         else if(dp[idx]!=-1) return dp[idx];
         int pick=nums[idx] + houseRobber(idx-2,dp,nums);
         int nopick=0 + houseRobber(idx-1,dp,nums);
