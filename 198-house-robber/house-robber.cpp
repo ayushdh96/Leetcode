2515c62c64cc75f52a888b9e860e158aa147e9e8
Ayush Dhoundiyal <ayushdh96@gmail.com>
2025-10-11 12:31:53 -0400

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.53%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+private:
+    int houseRobber(int idx, vector<int>& dp, vector<int>& nums){
+        if(idx<0) return 0;
+        else if(idx==0) return nums[idx];
+        else if(dp[idx]!=-1) return dp[idx];
+        int pick=nums[idx] + houseRobber(idx-2,dp,nums);
+        int nopick=0 + houseRobber(idx-1,dp,nums);
+        return dp[idx]=max(pick,nopick);
+    }
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return houseRobber(n-1,dp,nums);
+    }
+};
