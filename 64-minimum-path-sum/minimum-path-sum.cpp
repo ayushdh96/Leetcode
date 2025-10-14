0a8e79fd3de15859d8d1fbf96d1e89382a0767ee
Ayush Dhoundiyal <ayushdh96@gmail.com>
2025-10-14 19:02:59 -0400

    Time: 3 ms (31.86%) | Memory: 16.3 MB (40.28%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -14,6 +14,20 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return movement(m-1,n-1,dp,grid);
+        //return movement(m-1,n-1,dp,grid);
+        //writing the code for the tabulation
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]=grid[i][j];
+                else{
+                    int up=INT_MAX;
+                    int right=INT_MAX;
+                    if(i>0)  up=grid[i][j]+dp[i-1][j];
+                    if(j>0)  right=grid[i][j]+dp[i][j-1];
+                    dp[i][j]=min(up,right);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
