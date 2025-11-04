4c5923cebc38be9346d57588cc15a233b0d87a86
Ayush Dhoundiyal <ayushdh96@gmail.com>
2025-11-03 22:37:09 -0500

    Time: 47 ms (34.81%) | Memory: 48.8 MB (43.88%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -15,9 +15,23 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         if(amount==0) return 0;
         int length=coins.size();
-        vector<vector<int>> dp(length,vector<int>(amount+1,-1));
-        int ans=recur(length-1,amount,coins,dp);
-        if (ans>amount) return -1;
-        else return ans;
+        //vector<vector<int>> dp(length,vector<int>(amount+1,-1));
+        //int ans=recur(length-1,amount,coins,dp);
+        //if (ans>amount) return -1;
+        //else return ans;
+        vector<vector<int>> dp(length,vector<int>(amount+1,1e9));
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0) dp[0][i] = i/coins[0];
+        }
+        for(int index=1;index<length;index++){
+            for(int target=0;target<=amount;target++){
+                int noTake=dp[index-1][target];
+                int take=1e9;
+                if(coins[index]<=target) take=1+dp[index][target-coins[index]];
+                dp[index][target]=min(take,noTake);
+            }
+        }
+        if (dp[length-1][amount]>amount) return -1;
+        else return dp[length-1][amount];
     }
 };
