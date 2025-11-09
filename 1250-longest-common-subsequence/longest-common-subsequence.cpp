54a2a1b17649ef6814d8326f10715c7fc2774a35
Ayush Dhoundiyal <ayushdh96@gmail.com>
2025-11-09 16:21:08 -0500

    Time: 47 ms (10.88%) | Memory: 27.7 MB (5.97%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,9 +1,9 @@
 class Solution {
 private:
     int recur(int index1,int index2,string& text1, string& text2, vector<vector<int>>& dp){
-        if(index1<0 || index2<0) return 0;
+        if(index1==0 || index2==0) return 0;
         if(dp[index1][index2]!=-1) return dp[index1][index2];
-        if(text1[index1]==text2[index2]) return dp[index1][index2]=1+recur(index1-1,index2-1,text1,text2,dp);
+        if(text1[index1-1]==text2[index2-1]) return dp[index1][index2]=1+recur(index1-1,index2-1,text1,text2,dp);
         else{
             int left=recur(index1-1,index2,text1,text2,dp);
             int right=recur(index1,index2-1,text1,text2,dp);
@@ -15,6 +15,6 @@ public:
         int length1=text1.size();
         int length2=text2.size();
         vector<vector<int>> dp(length1+1,vector<int>(length2+1,-1));
-        return recur(length1-1,length2-1,text1,text2,dp);
+        return recur(length1,length2,text1,text2,dp);
     }
 };
