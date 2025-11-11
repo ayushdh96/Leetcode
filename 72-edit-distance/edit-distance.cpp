84a6fbadfa5007e9e67a00b4283951df0cd52cde
Ayush Dhoundiyal <ayushdh96@gmail.com>
2025-11-11 13:49:41 -0500

    Time: 3 ms (91.45%) | Memory: 10.4 MB (95.41%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -22,7 +22,7 @@ public:
         int length2=word2.size();
         //vector<vector<int>> dp(length1+1,vector<int>(length2+1,-1));
         //return recur(length1,length2,word1,word2,dp);
-        vector<vector<int>> dp(length1+1,vector<int>(length2+1,0));
+        /**vector<vector<int>> dp(length1+1,vector<int>(length2+1,0));
         for(int i=0;i<=length1;i++) dp[i][0]=i;
         for(int i=0;i<=length2;i++) dp[0][i]=i;
         for(int index1=1;index1<=length1;index1++){
@@ -39,6 +39,25 @@ public:
                 }
             }
         }
-        return dp[length1][length2];
+        return dp[length1][length2];**/
+        vector<int> curr(length2+1,0),prev(length2+1,0);
+        for(int i=0;i<=length2;i++) prev[i]=i;
+        for(int index1=1;index1<=length1;index1++){
+            curr[0]=index1;
+            for(int index2=1;index2<=length2;index2++){
+                if(word1[index1-1]==word2[index2-1])curr[index2]=prev[index2-1];
+        
+                else{
+                    int minV=1e9;
+                    int insert=1+curr[index2-1];
+                    int deleteC=1+prev[index2];
+                    int replace=1+prev[index2-1];
+                    minV=min(insert,replace);
+                    curr[index2]=min(deleteC,minV);
+                }
+            }
+            prev=curr;
+        }
+        return prev[length2];
     }
 };
