ab0711fcef3a5640672c05d60bdae48144760c6b
Ayush Dhoundiyal <ayushdh96@gmail.com>
2025-11-12 11:48:29 -0500

    Time: 51 ms (29.07%) | Memory: 54.8 MB (6.34%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    bool recur(int index1, int index2, string& s, string& p, vector<vector<int>>& dp){
+        //base case
+        if(index1==0 && index2==0) return true;
+        if(index2==0) return false;
+        if(index1==0 && index2>0){
+            for(int i=0;i<=index2-1;i++){
+                if(p[i]!='*') return false;
+            }
+            return true;
+        }
+        if(dp[index1][index2]!=-1) return dp[index1][index2];
+        if(s[index1-1]==p[index2-1] || p[index2-1]=='?') return dp[index1][index2]=recur(index1-1,index2-1,s,p,dp);
+        else if(p[index2-1]=='*'){
+            bool noConsider=recur(index1,index2-1,s,p,dp);
+            bool stay=recur(index1-1,index2,s,p,dp);
+            return dp[index1][index2]=(noConsider || stay);
+        }
+        else {
+            return false;
+        }
+    }
+public:
+    bool isMatch(string s, string p) {
+        int length1=s.size();
+        int length2=p.size();
+        vector<vector<int>> dp(length1+1,vector<int>(length2+1,-1));
+        return recur(length1,length2,s,p,dp);
+    }
+};
