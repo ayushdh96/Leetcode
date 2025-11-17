76a5a772e932f607ff287c5ad75d3a172ee8571f
Ayush Dhoundiyal <ayushdh96@gmail.com>
2025-11-17 10:18:45 -0500

    Time: 0 ms (100.00%) | Memory: 14.1 MB (78.53%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -14,7 +14,7 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         //vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,-1));
         //return recur(0,-1,nums,dp);
-        vector<vector<int>> dp(nums.size()+1,vector<int>(nums.size()+1,0));
+        /**vector<vector<int>> dp(nums.size()+1,vector<int>(nums.size()+1,0));
         for(int index=nums.size()-1;index>=0;index--){
             for(int prev=index-1;prev>=-1;prev--){
                 int length=dp[index+1][prev+1];
@@ -24,6 +24,16 @@ public:
                 else dp[index][prev+1]=length;
             }
         }
-        return dp[0][0];
+        return dp[0][0];**/
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>temp.back()) temp.push_back(nums[i]);
+            else{
+                int index=lower_bound(temp.begin(),temp.end(),nums[i]) - temp.begin();
+                temp[index]=nums[i];
+            }
+        }
+        return temp.size();
     }
 };
